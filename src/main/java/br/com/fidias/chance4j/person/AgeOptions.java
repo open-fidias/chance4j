d81e39edca79fa438b362d5e8080371abfe6d520
Átila Camurça <camurca.home@gmail.com>
2016-08-11 09:08:13 -0300

    Generate a random age.
    Fixed #5.

---
+++ src/main/java/br/com/fidias/chance4j/person/AgeOptions.java
@@ -0,0 +1,50 @@
+/**
+ * Chance4j is a minimalist generator of random strings, numbers, etc. to
+ * help reduce some monotony particularly while writing automated tests or
+ * anywhere else you need anything random.
+ * Based on the <http://chancejs.com> by Victor Quinn and contributors
+ *
+ * Copyright (C) 2016 Átila Camurça <camurca.home@gmail.com>
+ * Fidias Free Source Team <fidiascom@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package br.com.fidias.chance4j.person;
+
+/**
+ *
+ * @author atila
+ */
+public enum AgeOptions {
+    child(0, 12),
+    teen(13, 19),
+    adult(20, 65),
+    senior(65, 110),
+    any(0, 110);
+    
+    private final int min, max;
+    
+    private AgeOptions(int min, int max) {
+        this.min = min;
+        this.max = max;
+    }
+    
+    public int getMin() {
+        return this.min;
+    }
+    
+    public int getMax() {
+        return this.max;
+    }
+}
